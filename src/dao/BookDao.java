d146ebf874e1021968f2363fbf75596babd34e44
chenxing <1285258619@qq.com>
2017-05-25 16:11:32 +0800

    更新bookdao里面的错误

--- src/dao/BookDao.java
+++ src/dao/BookDao.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import conn.DBConnection;
+import common.DBConnection;
 import model.BookBean;
 
 public class BookDao {
